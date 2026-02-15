c32cdfb7c6a725e0135571504e7de3140406e0fd
Geetanjali65 <geetanjali65@example.com>
2026-02-15 17:53:43 +0530

    Spacing

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -284,12 +284,6 @@ export default function Projects() {
               </svg>
               GitHub
             </a>
-            <a href="#" target="_blank" rel="noopener noreferrer" className="btn btn-outline">
-              <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
-                <path d="M14,2H6A2,2 0 0,0 4,4V20A2,2 0 0,0 6,22H18A2,2 0 0,0 20,20V8L14,2M18,20H6V4H13V9H18V20Z"/>
-              </svg>
-              Architecture
-            </a>
           </div>
           
           {/* Auto-Sliding Gallery */}
